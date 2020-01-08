588886937267ea4f3dedd6c69e43c94150f07687
joshuashewmaker <jshewmak@gmail.com>
2020-01-07 19:13:31 -0500

    created routes file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:frequencypay/pages/home_page.dart';
-import 'package:frequencypay/pages/login_page.dart';
 import 'package:frequencypay/pages/splash_page.dart';
-import 'package:frequencypay/pages/login_signup_page.dart';
-import 'package:frequencypay/pages/task.dart';
+import 'package:frequencypay/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,11 +15,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green,
         ),
         home: SplashPage(),
-        routes: <String, WidgetBuilder>{
-          '/task': (BuildContext context) => TaskPage(title: 'Task'),
-          '/home': (BuildContext context) => HomePage(title: 'Home'),
-          '/login': (BuildContext context) => LoginPage(),
-          '/register': (BuildContext context) => RegisterPage(),
-        });
+        routes: routes,
+    );
   }
 }
