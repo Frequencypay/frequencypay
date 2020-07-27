a1dac18b8e8cf4b9c889f7462f890d0f0dc979c0
Mohamad H. Darwiche <53587691+MHDDARWIC@users.noreply.github.com>
2020-07-27 19:38:20 -0400

    profile picture

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +13,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart';
 
 void main() => runApp(MaterialApp(
-      home: ProfileScreen(),
-    ));
+  home: ProfileScreen(),
+));
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -25,9 +26,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
   static const Color blueHighlight = const Color(0xFF3665FF);
 
   File _image;
+  String _uploadedFileUrl;
   ProfileBloc createBloc(
-    var context,
-  ) {
+      var context,
+      ) {
     final user = Provider.of<User>(context, listen: false);
 
     ProfileBloc bloc = ProfileBloc(FirestoreService(uid: user.uid));
@@ -40,23 +42,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
 
-    Future getImage() async{
-      var image= await ImagePicker.pickImage(source: ImageSource.gallery);
-      setState(() {
-        _image=image;
-      });
-    }
-
-    Future uploadPic(BuildContext context) async{
-      String filename=basename(_image.path);
-      StorageReference firebaseStorageRef=FirebaseStorage.instance.ref().child(filename);
-      StorageUploadTask uploadTask=firebaseStorageRef.putFile(_image);
-      StorageTaskSnapshot taskSnapshot=await uploadTask.onComplete;
-      setState(() {
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text("Profile Picture Uploaded"),));
-      });
-
-    }
     return BlocProvider(
       create: (context) => createBloc(context),
       child: Scaffold(
@@ -83,163 +68,139 @@ class _ProfileScreenState extends State<ProfileScreen> {
             elevation: 0,
           ),
           body: ListView(
-        children: <Widget>[
-          Column(
             children: <Widget>[
-              SizedBox(
-                height: 20,
-              ),
-              SafeArea(
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      "  Your ",
-                      style: TextStyle(color: Colors.grey, fontSize: 30),
-                    ),
-                    Text(
-                      "Profile ",
-                      style: TextStyle(color: Colors.blue, fontSize: 30),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
+              Column(
+                children: <Widget>[
 
-                    CircleAvatar(
-                        radius: 60,
-                        backgroundColor: Color(0xff476cfb),
-                        child: ClipOval(
-                          child: SizedBox(
-                            width: 180.0,
-                            height: 180.0 ,
-                            child:(_image !=null)?Image.file(_image,fit:BoxFit.fill):Icon(Icons.person,size: 80.0,) ,
-                          ),
-                        )
-                    ),
+                  SizedBox(
+                    height: 30,
+                  ),
 
-                    Padding(
-                      padding: EdgeInsets.only(top:60.0),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.camera_alt,
-                          size: 30.0,
-                        ),
-                        onPressed: (){
-                          getImage();
-                        },
-                      ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    child: profileInfo(),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    "Confidence Rating",
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    child: getConfidenceRating(),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    child: getMailandPhone(),
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Edit",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
                     ),
-
-                    Padding(
-                      padding: EdgeInsets.only(top:60.0),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.check,
-                          size: 30.0,
-                        ),
-                        onPressed: (){
-                          uploadPic(context);
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: profileInfo(),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                "Confidence Rating",
-                style: TextStyle(color: Colors.black54, fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: getConfidenceRating(),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                child: getMailandPhone(),
-              ),
-              RaisedButton(
-                child: Text(
-                  "Edit",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                color: Colors.grey,
-                onPressed: () {},
+                    color: Colors.grey,
+                    onPressed: () {},
+                  ),
+                ],
               ),
             ],
-          ),
-        ],
-      )),
+          )),
     );
   }
 
-  Widget profileImg() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-
-        CircleAvatar(
-          radius: 60,
-          backgroundColor: Color(0xff476cfb),
-          child: ClipOval(
-            child: SizedBox(
-              width: 180.0,
-              height: 180.0 ,
-              child:(_image !=null)?Image.file(_image,fit:BoxFit.fill):Icon(Icons.person,size: 80.0,) ,
-            ),
-          )
-        ),
-
-        Padding(
-          padding: EdgeInsets.only(top:60.0),
-          child: IconButton(
-            icon: Icon(
-              Icons.camera_alt,
-              size: 30.0,
-            ),
-            onPressed: (){
-              //getImage();
-            },
-          ),
-        ),
-
-        Padding(
-          padding: EdgeInsets.only(top:60.0),
-          child: IconButton(
-            icon: Icon(
-              Icons.check,
-              size: 30.0,
-            ),
-            onPressed: (){
-              //uploadPic(context);
-            },
-          ),
-        )
-      ],
-    );
-  } // end profileImg
 
   Widget profileInfo() {
+    Future getImage() async{ //get an image from gallery
+      await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {
+        setState(() {
+          _image = image;
+        });
+      });
+    }
+
+
+    Future uploadFile() async {
+      String fileName = basename(_image.path);
+      StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName);
+      StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+      await uploadTask.onComplete;
+      print('File Uploaded');
+      firebaseStorageRef.getDownloadURL().then((fileURL) {
+        setState(() {
+          _uploadedFileUrl = fileURL; // to download late
+          //ASSIGN AS FIELD
+        });
+      });
+    }
+
+
     return Column(
       children: <Widget>[
+        //profile image
+        BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+          if (state is ProfileIsLoadedState) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.blue,
+                  child: ClipOval(
+                    child: SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: (_image!=null)? Image.file(_image, fit: BoxFit.fill,):Text("No Profile Image was set yet"),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(top:60.0),
+                  child: IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    onPressed: () async{
+                      getImage();
+                    },
+                  ),
+                ),
+
+                RaisedButton(
+                  child: Text("Submit"),
+                  onPressed: () async{
+                    String fileName = state.getProfile.uid; //file name is the same as uid
+                    StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child(fileName);
+                    StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
+                    StorageTaskSnapshot taskSnapshot=await uploadTask.onComplete;
+                    setState(() {
+                      print("Profile Picture uploaded");
+                      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
+                    });
+                  },
+                )
+              ],
+            );
+          } else if (state is ProfileIsNotLoadedState) {
+            return Text(
+              "error",
+              style: TextStyle(color: Colors.black54, fontSize: 25),
+            );
+          } else {
+            return Text(
+              "<<fname>> <<lname>>",
+              style: TextStyle(color: Colors.black54, fontSize: 25),
+            );
+          }
+        }),
+
+
         BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
           if (state is ProfileIsLoadedState) {
             return Text(
@@ -395,3 +356,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 }
+
